304d736fffd84289fc8edc90b5d2542e98914ca7
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-03 11:04:49 -0500

    Displaying products

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ require('dotenv').config('env');
 const baseEndpoint = `https://api.spoonacular.com/food/products/`;
 const proxy = `https://cors-anywhere.herokuapp.com/`
 const form = document.querySelector('form.search');
+const recipesGrid = document.querySelector('.recipes');
 
 async function fetchRecipes(query){
   const res = await fetch(`${proxy}${baseEndpoint}search?query=${query}&apiKey=${process.env.API_KEY}`, {
@@ -17,23 +18,24 @@ async function handleSubmit(e){
   e.preventDefault();
   const el = e.currentTarget;
   //turn the form off
-  el.submit.disabled = false;
+  el.submit.disabled = true;
   //submitting the search
   const recipes = await fetchRecipes(el.query.value);
   console.log(recipes);
+  el.submit.disabled = false;
   displayRecipes(recipes.products);
 
 };
 
 function displayRecipes(recipes){
   console.log('Creating the HTML');
-  // const html = recipes.map(recipe =>  `
-  //     <div>
-  //       <h2>${recipe.title}<h2>
-  //       <image>${recipe.image}</p>
-  //     </div>`
-  // );
-  // console.log(html);
+  const html = recipes.map(recipe =>  `
+      <div>
+        <h2>${recipe.title}<h2>
+        ${recipe.image && `<img src="${recipe.image}" alt="${recipe.title}"/>`}
+      </div>`
+  );
+  recipesGrid.innerHTML = html.join(' ');
 };
 
 form.addEventListener('submit', handleSubmit);
