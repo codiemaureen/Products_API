fb9f13dd773c4fc736efa4f0a50f65c090f52959
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-04 11:02:10 -0500

    Updating Variables

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 const baseEndpoint = `https://api.spoonacular.com/food/products/`;
 const proxy = `https://cors-anywhere.herokuapp.com/`
 const form = document.querySelector('form.search');
-const recipesGrid = document.querySelector('.recipes');
-require('dotenv').config('env');
+const productsGrid = document.querySelector('.products');
+require("dotenv").config();
 
 
-async function fetchRecipes(query){
+async function fetchProducts(query){
   //response
   const res = await fetch(`${proxy}${baseEndpoint}search?query=${query}&apiKey=${process.env.API_KEY}`, {
     //converts data to JSON
@@ -28,24 +28,24 @@ async function fetchAndDisplay(query){
     //turn the form off
   form.submit.disabled = true;
   //submitting the search
-  const recipes = await fetchRecipes(query);
-  console.log(recipes);
+  const products = await fetchProducts(query);
+  console.log(products);
   //turn the form on
   form.submit.disabled = false;
-  displayRecipes(recipes.products);
+  displayProducts(products.products);
   
 };
 
-function displayRecipes(recipes){
+function displayProducts(products){
   console.log('Creating the HTML');
-  const html = recipes.map((recipe) =>  `
+  const html = products.map((product) =>  `
       <div>
         <h2>${recipe.title}<h2>
         ${recipe.image && `<img src="${recipe.image}" alt="${recipe.title}"/>`}
       </div>`
   );
   //displaying Products as a grid
-  recipesGrid.innerHTML = html.join(' ');
+  productsGrid.innerHTML = html.join(' ');
 };
 
 
